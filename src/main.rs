641c2056f9cd68835e9e612ad626ee72fadc3be4
Al Cheung <cangzan@gmail.com>
2023-03-17 18:37:22 +0800

    change: json resp.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Json, Router};
+use serde_json::{json, Value};
+
+async fn json() -> Json<Value> {
+    Json(json!({ "data": 42 }))
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+    let app = Router::new().route("/", get(json));
 
     axum::Server::bind(&"0.0.0.0:5050".parse().unwrap())
         .serve(app.into_make_service())
