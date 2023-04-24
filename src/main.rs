142a2dc1eb4567f441d4617ecd1e29e8d41da124
Al Cheung <cangzan@gmail.com>
2023-04-25 00:07:49 +0800

    chore: reorganize

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,18 @@
-pub mod errors;
-pub mod users;
-
 use std::{env, net::SocketAddr};
 
 use axum::{
-    http::StatusCode,
     routing::{get, post},
     Extension, Json, Router,
 };
 use serde_json::{json, Value};
 use sqlx::SqlitePool;
 
+mod controllers;
+mod errors;
+mod models;
+
 async fn json() -> Json<Value> {
     Json(json!({ "data": 42 }))
-}
-
-async fn create_user(
-    Json(form): Json<users::CreateUserForm>,
-    Extension(db_pool): Extension<SqlitePool>,
-) -> Result<(StatusCode, Json<users::User>), errors::CustomError> {
-    let user = sqlx::query_as!(
-        users::User,
-        r#"INSERT INTO users (name, active) VALUES(?, 1)"#,
-        form.name
-    )
-    .execute(&db_pool)
-    .await
-    .map_err(|_| errors::CustomError::InternalServerError)?;
-
-    let user_id = user.last_insert_rowid();
-    let user = sqlx::query_as!(
-        users::User,
-        r#"SELECT id, name, active FROM users where id = ?"#,
-        user_id
-    )
-    .fetch_one(&db_pool)
-    .await
-    .map_err(|_| errors::CustomError::InternalServerError)?;
-
-    Ok((StatusCode::CREATED, Json(user)))
 }
 
 #[tokio::main]
@@ -48,8 +22,8 @@ async fn main() -> anyhow::Result<()> {
     let db_pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
 
     let app = Router::new()
-        .route("/", get(json))
-        .route("/api/user", post(create_user))
+        .route("/api/ping", get(json))
+        .route("/api/user", post(controllers::users::create_user))
         .layer(Extension(db_pool));
     let addr = SocketAddr::from(([127, 0, 0, 1], 5050));
     tracing::debug!("Listening on {}", addr);
