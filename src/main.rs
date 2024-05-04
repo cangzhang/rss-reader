49b5a95c93088965a64c1d6274ef1404fae9edd9
Al <cangzan@gmail.com>
2024-05-04 21:26:08 +0800

    test route

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use std::{env, net::SocketAddr};
+use std::env;
 
-use axum::{
-    routing::{get, post},
-    Extension, Json, Router,
-};
+use axum::{routing::get, Extension, Json, Router};
 use serde_json::{json, Value};
-
-async fn json() -> Json<Value> {
-    Json(json!({ "data": 42 }))
-}
+use tokio_rusqlite::{params, Connection};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -18,12 +12,28 @@ async fn main() -> anyhow::Result<()> {
     // let host = env::var("HOST").expect("HOST is not set in .env file");
     let port = env::var("PORT").expect("PORT is not set in .env file");
     let server_url = format!("0.0.0.0:{port}");
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let conn = Connection::open(&db_url).await?;
 
-    let app = Router::new().nest("/api", Router::new().route("/ping", get(json)));
+    let app = Router::new()
+        .nest("/api", Router::new().route("/ping", get(ping)))
+        .layer(Extension(&conn));
 
     let listener = tokio::net::TcpListener::bind(&server_url).await.unwrap();
     axum::serve(listener, app).await?;
     tracing::debug!("Listening on {}", server_url);
 
     Ok(())
+}
+
+async fn ping(Extension(conn): Extension<&Connection>) -> anyhow::Result<Json<Value>> {
+    conn.call(|conn| {
+        Ok(conn.execute(
+            "INSERT INTO feed (url, name) VALUES (?1, ?2)",
+            params![&"https://example.com", &"Example"],
+        )?)
+    })
+    .await?;
+
+    Ok(Json(json!({"message": "Data inserted successfully"})))
 }
